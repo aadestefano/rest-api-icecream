0db1b9bcfc1eb8f2123059159540c422bc297f66
aadestefano <anthonyangelodestefano@gmail.com>
2024-12-02 19:48:52 -0500

    uploading

--- addFlavor.py
+++ addFlavor.py
@@ -11,6 +11,18 @@ def addFlavor(flavorName,flavorDesc):
     #adding a flavor to the list
     iceCreamFlavors.append(flavor)
 
+#adding an update function
+
+def updateFlavor(oldFlavor, newFlavor, newDesc):
+    #locate flavor by old name
+    for flavor in iceCreamFlavors:
+        if flavor['name'] == oldFlavor:
+            #updating the name
+            flavor['name'] = newFlavor
+            flavor['description'] = newDesc
+            return True #checks if update went through
+        return False #when flavor is not found
+
 def displayFlavors():
     #display the flavors in the list
     for flavor in iceCreamFlavors:
@@ -20,6 +32,26 @@ def displayFlavors():
 addFlavor('Chocolate Fudge', 'Sweet cocoa with fudge')
 addFlavor('Strawberry Banana', 'Sweet & fruity')
 addFlavor('Cookies & Cream', 'Vanilla & chocolate with chocolate cookie chunks')
+
+def inputAddFlavor():
+    flavorName = input("Enter flavor name: ")
+    flavorDesc = input("Enter flavor description: ")
+    addFlavor(flavorName, flavorDesc)
+    print(f"Flavor: {flavorName} , added successfully.")
+
+while True:
+    inputAddFlavor()
+    addedFlavor = input("Would you like to add another flavor? (y/n): ")
+    if addedFlavor != 'y':
+        break
+
+
+
+#if updateFlavor('Chocolate Fudge', 'Mint Chocolate', 'Minty with chocolate chips'):
+#    print("Flavor updated!")
+#else:
+#    print("Flavor not updated!")
+
 
 displayFlavors()
 
